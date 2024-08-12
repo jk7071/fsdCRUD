da99021a158daf8641b626745b20e49b0a17b484
jk7071 <jiteshk7071@gmail.com>
2024-08-12 21:24:18 +0530

    mongodb replica updated

--- server/app.js
+++ server/app.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const url = 'mongodb://localhost/CBIT'
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const app = express()
 
-mongoose.connect(url, { useNewUrlParser: true })
-const con = mongoose.connection
+const url = 'mongodb://localhost:27020,localhost:27021,localhost:27022/cbititl?replicaSet=m101';
 
+const app = express();
+
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const con = mongoose.connection;
 con.on('open', () => {
-    console.log('connected...')
-})
+    console.log('Connected to MongoDB...');
+});
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-const alienRouter = require('./routes/aliens')
-app.use('/aliens', alienRouter)
+const alienRouter = require('./routes/route');
+app.use('/cbitit3', alienRouter);
 
 app.listen(9000, () => {
-    console.log('Server started')
-})
+    console.log('Server started on port 9000');
+});
